9250201b3bd8408660eb6d9a02f0e87f3ab96e27
= <=>
2020-08-26 22:03:59 -0300

    atualizando

--- tests/Feature/Rules/GenresHasCategoriesRuleTest.php
+++ tests/Feature/Rules/GenresHasCategoriesRuleTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Genre;
+use App\Rules\GenresHasCategoriesRule;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
@@ -26,5 +27,55 @@ class GenresHasCategoriesRuleTest extends TestCase
         $this->genres[1]->categories()->sync([
             $this->categories[2]->id
         ]);
+    }
+
+    public function testPassesIsValid()
+    {
+        $rule = new GenresHasCategoriesRule([
+            $this->categories[2]->id
+        ]);
+        $isValid = $rule->passes('', [$this->genres[1]->id]);
+        $this->assertTrue($isValid);
+
+        $rule = new GenresHasCategoriesRule([
+            $this->categories[0]->id,
+            $this->categories[2]->id
+        ]);
+        $isValid = $rule->passes('', [
+            $this->genres[0]->id,
+            $this->genres[1]->id
+        ]);
+        $this->assertTrue($isValid);
+
+        $rule = new GenresHasCategoriesRule([
+            $this->categories[0]->id,
+            $this->categories[1]->id,
+            $this->categories[2]->id
+        ]);
+        $isValid = $rule->passes('', [
+            $this->genres[0]->id,
+            $this->genres[1]->id
+        ]);
+        $this->assertTrue($isValid);
+    }
+
+    public function testPassesIsInvalid()
+    {
+        $rule = new GenresHasCategoriesRule([
+            $this->categories[0]->id
+        ]);
+        $isValid = $rule->passes('', [
+            $this->genres[0]->id,
+            $this->genres[1]->id
+        ]);
+        $this->assertFalse($isValid);
+
+        $rule = new GenresHasCategoriesRule([
+            $this->categories[3]->id
+        ]);
+        $isValid = $rule->passes('', [
+            $this->genres[0]->id
+        ]);
+        $this->assertFalse($isValid);
     }
 }
