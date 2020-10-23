c945e51ab1f0f493d37646ebc44e9a5479f3485f
= <=>
2020-10-23 18:34:18 -0300

    alterações no cabeçalho

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,12 +1,73 @@
-import * as React from 'react';
-import { AppBar, Toolbar } from '@material-ui/core';
+import * as React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  makeStyles,
+  Theme,
+  IconButton,
+  Menu,
+  MenuItem
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import logo from "../../static/img/logo.png";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  toolbar: {
+    backgroundColor: "#000000",
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: "center",
+  },
+  logo: {
+    width: 100,
+    [theme.breakpoints.up("sm")]: {
+      width: 170,
+    },
+  },
+}));
 
 export const Navbar: React.FC = () => {
-    return (
-        <AppBar>
-            <Toolbar>
-                Texto
-            </Toolbar>
-        </AppBar>
-    );
-}
+  const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleOpen = (event:any) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
+
+  return (
+    <AppBar>
+      <Toolbar className={classes.toolbar}>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleOpen}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        <Menu 
+            id="menu-appbar"
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+            transformOrigin={{vertical: 'top', horizontal: 'center'}}
+            getContentAnchorEl={null}
+          >
+              <MenuItem onClick={handleClose}>Categorias</MenuItem>
+           </Menu>
+        <Typography className={classes.title}>
+          <img src={logo} alt="Codeflix" className={classes.logo}></img>
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
